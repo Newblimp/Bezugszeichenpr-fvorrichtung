a5e68c89c40ce8a1ef2a6fe25dadd24d48813dc7
Newblimp <nicolo.novello@tum.de>
2023-04-24 00:52:35 +0200

    added images

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,10 +1,14 @@
 #include "MainWindow.h"
+#include "../img/check_16.xpm"
+#include "../img/warning_16.xpm"
 #include "utils.h"
 #include "wx/event.h"
+#include "wx/gdicmn.h"
 #include <algorithm>
 #include <locale>
 #include <memory>
 #include <string>
+#include <wx/bitmap.h>
 #include <wx/regex.h>
 
 MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "My App") {
@@ -24,15 +28,6 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "My App") {
       panel, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
   viewSizer->Add(m_textBox.get(), 1, wxEXPAND | wxALL, 10);
 
-  // Add a list control to the sizer
-  /*
-  m_listBox = std::make_shared<wxListCtrl>(panel, wxID_ANY, wxDefaultPosition,
-                                           wxDefaultSize, wxLC_REPORT);
-  m_listBox->InsertColumn(0, "Zeichen");
-  m_listBox->InsertColumn(1, "Merkmal", wxLIST_FORMAT_LEFT, 400);
-  viewSizer->Add(m_listBox.get(), 1, wxEXPAND | wxALL, 10);
-  */
-
   // Add a dataView to the sizer
   m_treeList = std::make_shared<wxTreeListCtrl>(
       panel, wxID_ANY, wxDefaultPosition, wxDefaultSize);
@@ -46,6 +41,9 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "My App") {
   wxBoxSizer *buttonSizer = new wxBoxSizer(wxHORIZONTAL);
   buttonSizer->Add(m_scanButton.get(), 0, wxCENTRE, 10);
   mainSizer->Add(buttonSizer, 0, wxEXPAND | wxALL, 10);
+
+  loadIcons();
+  m_treeList->SetImageList(m_imageList.get());
 
   m_scanButton->Bind(wxEVT_BUTTON, &MainWindow::scanText, this);
 
@@ -72,6 +70,7 @@ void MainWindow::scanText(wxCommandEvent &event) {
     }
   }
 
+  m_textBox->SetStyle(0, text.length(), m_textBox->GetDefaultStyle());
   printList();
   m_treeList->SetSortColumn(0);
 }
@@ -85,13 +84,56 @@ void MainWindow::printList() {
   // Iterate over every saved item and insert it
   for (const auto &[bezugszeichen, merkmale] : m_merkmale) {
     // At highest level, add all merkmale together
-    item = m_treeList->AppendItem(m_treeList->GetRootItem(), bezugszeichen);
+    if (areAllWordsSame(merkmale)) {
+      item = m_treeList->AppendItem(m_treeList->GetRootItem(), bezugszeichen, 0,
+                                    0);
+    } else {
+      makeBold(merkmale);
+      item = m_treeList->AppendItem(m_treeList->GetRootItem(), bezugszeichen, 1,
+                                    1);
+    }
     m_treeList->SetItemText(item, 1, merkmaleToString(merkmale));
 
     // Then add them all seperately on a lower level
     for (const auto &merkmal : merkmale) {
       auto row = m_treeList->AppendItem(item, "");
       m_treeList->SetItemText(row, 1, merkmal);
+    }
+  }
+}
+
+void MainWindow::loadIcons() {
+  m_imageList = std::make_shared<wxImageList>(16, 16, false, 0);
+  wxBitmap check(check_16_xpm);
+  wxBitmap warning(warning_16_xpm);
+
+  m_imageList->Add(check);
+  m_imageList->Add(warning);
+}
+
+void MainWindow::makeBold(const std::set<wxString> &strings) {
+  wxTextAttr style;
+  style.SetFontWeight(wxFONTWEIGHT_BOLD);
+  style.SetTextColour(*wxRED);
+  // Get the current text in the wxTestCtrl
+  wxString currentText;
+
+  int pos, offset{0};
+  for (const auto &string : strings) {
+    // Set up text and indices
+    currentText = m_textBox->GetValue();
+    pos = currentText.Find(string);
+    offset = 0;
+
+    // Loop the indices over the text
+    while (pos != wxNOT_FOUND) {
+      // Apply the red and bold style to the selected text
+      m_textBox->SetStyle(pos + offset, pos + string.length() + offset, style);
+
+      currentText = currentText.Mid(pos + string.length());
+      offset += pos + string.length();
+      // Find the position of the string in the text
+      pos = currentText.Find(string);
     }
   }
 }
