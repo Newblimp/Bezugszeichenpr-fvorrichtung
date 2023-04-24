8758c69c4154e16d47094fbc84b2e060af962041
Newblimp <nicolo.novello@tum.de>
2023-04-24 22:55:39 +0200

    removed scan button, added future suggestions window

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "BZComparator.h"
+#include "wx/textctrl.h"
 #include "wx/treelist.h"
 #include <map>
 #include <set>
@@ -21,9 +22,10 @@ private:
   void makeBold(const std::set<wxString> &strings);
 
   std::map<wxString, std::set<wxString>> m_merkmale;
-  std::shared_ptr<wxButton> m_scanButton;
+  // std::shared_ptr<wxButton> m_scanButton;
   std::shared_ptr<wxTextCtrl> m_textBox;
   std::shared_ptr<wxImageList> m_imageList;
+  std::shared_ptr<wxListCtrl> m_listBox;
   std::shared_ptr<wxTreeListCtrl> m_treeList;
   BZComparator m_BZComparator;
 };
