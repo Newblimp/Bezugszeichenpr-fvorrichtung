672580de2ecba896e188909cc1af508e296329f2
Newblimp <nicolo.novello@tum.de>
2023-05-02 23:25:16 +0200

    undid the richtext

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -1,14 +1,12 @@
 #pragma once
 #include "BZComparator.h"
 #include "wx/regex.h"
-// #include "wx/textctrl.h"
+#include "wx/textctrl.h"
 #include "wx/treelist.h"
 #include <map>
-#include <memory>
 #include <set>
 #include <wx/dataview.h>
 #include <wx/listctrl.h>
-#include <wx/richtext/richtextctrl.h>
 #include <wx/wx.h>
 
 class MainWindow : public wxFrame {
@@ -36,7 +34,7 @@ private:
   std::map<std::wstring, std::set<wxString>> m_merkmale_to_bz;
   std::set<wxString> m_all_merkmale;
   // std::shared_ptr<wxButton> m_scanButton;
-  std::shared_ptr<wxRichTextCtrl> m_textBox;
+  std::shared_ptr<wxTextCtrl> m_textBox;
   std::shared_ptr<wxImageList> m_imageList;
   std::shared_ptr<wxListCtrl> m_listBox;
   std::shared_ptr<wxTreeListCtrl> m_treeList;
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -24,9 +24,10 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "My App") {
   wxBoxSizer *outputSizer = new wxBoxSizer(wxVERTICAL);
 
   // Add a text box to the sizer
-  m_textBox = std::make_shared<wxRichTextCtrl>(panel);
-  // m_textBox = std::make_shared<wxRichTextCtrl>(
-  // panel, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxRE_MULTILINE);
+  m_textBox = std::make_shared<wxTextCtrl>(
+      // m_textBox = std::make_shared<wxRichTextCtrl>(
+      panel, wxID_ANY, "", wxDefaultPosition, wxDefaultSize,
+      wxRE_MULTILINE | wxTE_RICH2);
   // wxDefaultSize, wxTE_MULTILINE | wxTE_RICH);
   viewSizer->Add(m_textBox.get(), 1, wxEXPAND | wxALL, 10);
   viewSizer->Add(outputSizer, 1, wxEXPAND, 10);
